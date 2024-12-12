ce128ad608286b2893ba34a2326834ef37789933
Recep <recep.yildiz@hva.nl>
2024-12-13 00:58:07 +0100

    Bij toggle nu 'zie minder'
    Bij toggle nu zie minder

--- opdracht 2/index.js
+++ opdracht 2/index.js
@@ -2,8 +2,16 @@
 document.querySelector('.toggleButton').addEventListener('click', function () {
     // Haalt de tekst element op door de class te zoeken
     const verborgenText = document.querySelector('.verborgen, .showing-more');
+    const toggleButton = this; // Verwijst naar de knop die werd aangeklikt
     
     // Toggle de klassen voor zichtbaarheid
     verborgenText.classList.toggle('verborgen');
     verborgenText.classList.toggle('showing-more');
+
+        // Verandert de knoptekst
+        if (verborgenText.classList.contains('showing-more')) {
+            toggleButton.textContent = 'Zie Minder';
+        } else {
+            toggleButton.textContent = 'Zie Meer';
+        }
 });
